4241a60961231e90297f0c91cd7987d92c98ee98
Thomas Nichols <nichols7@nospam.thanks.gmail.com>
2006-03-08 11:24:26 +0000

    Trunk is being scrubbed (copied to branch mitch_initial) prior to commits from sprint started 2006-03-07.
    git-svn-id: https://svn.versiondude.net/railfrog/cms@29 02240113-d30a-0410-b920-55b7e5eb95ec

--- trunk/extensions/page/page_item.rb
+++
@@ -1,4 +0,0 @@
-class PageItem < ActiveRecord::Base
-  belongs_to :item
-  belongs_to :page, :foreign_key => 'page_id', :class_name => 'Item'
-end
--- trunk/lib/core_ext/erb_utils_ext.rb
+++
@@ -1,8 +0,0 @@
-class ERB
-  module Util
-    def html_unescape(s)
-      s.to_s.gsub(/&amp;/, "&").gsub(/&quot;/, "\"").gsub(/&gt;/, ">").gsub(/&lt;/, "<")
-    end
-    alias hu html_unescape
-  end
-end
