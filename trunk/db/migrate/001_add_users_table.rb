832643e427bb4283d69e94318909615a1a1e34d0
Yury Kotlyarov <yura@nospam.thanks.brainhouse.ru>
2006-03-13 13:34:07 +0000

    Ported to use migration instead of DDL. Changed names of datetime field (i.e. created_at instead of created_on)
    git-svn-id: https://svn.versiondude.net/railfrog/cms@33 02240113-d30a-0410-b920-55b7e5eb95ec

---
+++ trunk/db/migrate/001_add_users_table.rb
@@ -0,0 +1,23 @@
+class AddUsersTable < ActiveRecord::Migration
+  def self.up
+    create_table :users, :force => true do |t|
+      t.column :first_name, :string, :null => false
+      t.column :last_name,  :string, :null => false
+      t.column :email,      :string, :null => false
+      t.column :password,   :string, :limit => 40, :null => false
+      t.column :created_at, :datetime, :null => false, 
+        :default => '1970-01-01 00:00:00'
+      t.column :updated_at, :datetime, :null => false, 
+        :default => '1970-01-01 00:00:00'
+    end
+    
+    User.create :first_name => "Test", 
+      :last_name  => "Tester", 
+      :email      => "test@test.com", 
+      :password   => "5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8"
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
