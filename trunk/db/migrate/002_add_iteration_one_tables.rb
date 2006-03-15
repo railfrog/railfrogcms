3e1d57b5c3a5c9f5a1887598eb80f7c48990c077
Yury Kotlyarov <yura@nospam.thanks.brainhouse.ru>
2006-03-15 12:06:41 +0000

    last_updated_at renamed to updated_at
    git-svn-id: https://svn.versiondude.net/railfrog/cms@36 02240113-d30a-0410-b920-55b7e5eb95ec

--- trunk/db/migrate/002_add_iteration_one_tables.rb
+++ trunk/db/migrate/002_add_iteration_one_tables.rb
@@ -9,13 +9,13 @@ class AddIterationOneTables < ActiveRecord::Migration
       t.column :mime_type,       :string, :limit => 50
       t.column :author_email,    :string, :limit => 100
       t.column :content,         :binary
-      t.column :last_updated_at, :datetime
+      t.column :updated_at, :datetime
     end
 
     create_table :live_versions, :options => options do |t|
       t.column :chunk_id,        :integer
       t.column :version,         :integer
-      t.column :last_updated_at, :datetime
+      t.column :updated_at, :datetime
     end
     
     create_table :layouts, :options => options do |t|
@@ -27,7 +27,7 @@ class AddIterationOneTables < ActiveRecord::Migration
       t.column :live_version_id, :integer
       t.column :version,         :integer
       t.column :layout_id,       :integer
-      t.column :last_updated_at, :datetime
+      t.column :updated_at, :datetime
     end
     add_index :site_mappings, :path, :unique
     
