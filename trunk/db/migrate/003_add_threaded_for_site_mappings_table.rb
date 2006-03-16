2eaa9121b103303a97e3bec3246272038de7e65d
Yury Kotlyarov <yura@nospam.thanks.brainhouse.ru>
2006-03-16 23:38:22 +0000

    added indecies for acts_as_threaded
    git-svn-id: https://svn.versiondude.net/railfrog/cms@42 02240113-d30a-0410-b920-55b7e5eb95ec

--- trunk/db/migrate/003_add_threaded_for_site_mappings_table.rb
+++ trunk/db/migrate/003_add_threaded_for_site_mappings_table.rb
@@ -5,6 +5,10 @@ class AddThreadedForSiteMappingsTable < ActiveRecord::Migration
     add_column :site_mappings, :depth, :integer
     add_column :site_mappings, :lft, :integer
     add_column :site_mappings, :rgt, :integer
+    
+    add_index :site_mappings, :path_segment
+    add_index :site_mappings, :lft
+    add_index :site_mappings, :rgt
   end
 
   def self.down
