dfcf3d35d773d75fd3e4a13db695cf206f3e8f2c
Yury Kotlyarov <yura@nospam.thanks.brainhouse.ru>
2006-03-18 19:28:33 +0000

    Fixed method name
    git-svn-id: https://svn.versiondude.net/railfrog/cms@48 02240113-d30a-0410-b920-55b7e5eb95ec

--- trunk/app/controllers/site_mapper_controller.rb
+++ trunk/app/controllers/site_mapper_controller.rb
@@ -3,7 +3,7 @@ class SiteMapperController < ApplicationController
   # Unit tests are required for different urls:
   # '', 'aaa', 'aaa/bbb', etc.
   def show_chunk
-    @chunk_version, @layout = SiteMapping.find_chunk(@params[:path])
+    @chunk_version, @layout = SiteMapping.find_chunk_and_layout(@params[:path])
     redirect_to :action => 'notfound' unless @chunk_version
   end
   
