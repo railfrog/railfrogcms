198fc188dcf0c5316e33cf5fcec7993c63d17cf3
Yury Kotlyarov <yura@nospam.thanks.brainhouse.ru>
2006-03-21 15:03:09 +0000

    Added ability to show images - expect inline in admin interface
    git-svn-id: https://svn.versiondude.net/railfrog/cms@77 02240113-d30a-0410-b920-55b7e5eb95ec

--- trunk/app/controllers/site_mapper_controller.rb
+++ trunk/app/controllers/site_mapper_controller.rb
@@ -3,6 +3,7 @@ class SiteMapperController < ApplicationController
   
   def show_chunk
     @chunk_version, @layout = SiteMapping.find_chunk_and_layout(@params[:path])
+    send_data @chunk_version.content, :filename => @params[:path].to_s, :type => @chunk_version.chunk.mime_type, :disposition => 'inline' if @chunk_version && @chunk_version.chunk && @chunk_version.chunk.is_binary?
     redirect_to :action => 'notfound' unless @chunk_version
   end
   
--- trunk/app/models/chunk.rb
+++ trunk/app/models/chunk.rb
@@ -1,6 +1,10 @@
 class Chunk < ActiveRecord::Base
   has_many :chunk_versions
   
+  def is_binary?
+    self.mime_type && self.mime_type.include?("image")
+  end
+  
   def find_version(version = nil)
     Chunk.find_version(self.id, version)
   end
