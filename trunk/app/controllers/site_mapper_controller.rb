22b1b1a1f6c1e7cd3eef18a61dcb2ab06238e3e9
Yury Kotlyarov <yura@nospam.thanks.brainhouse.ru>
2006-03-30 13:58:47 +0000

    Better code to show 404
    git-svn-id: https://svn.versiondude.net/railfrog/cms@85 02240113-d30a-0410-b920-55b7e5eb95ec

--- trunk/app/controllers/site_mapper_controller.rb
+++ trunk/app/controllers/site_mapper_controller.rb
@@ -2,15 +2,19 @@ class SiteMapperController < ApplicationController
   layout 'default'
   
   def show_chunk
-    @chunk_version, @layout = SiteMapping.find_chunk_and_layout(@params[:path])
+    @chunk_version, @layout, @site_mapping = SiteMapping.find_chunk_and_layout(@params[:path])
     
-    mime_type = @chunk_version.chunk.mime_type.mime_type if @chunk_version.chunk.mime_type
-    send_data @chunk_version.content, :filename => @params[:path].pop, :type => mime_type, :disposition => 'inline' if @chunk_version && @chunk_version.chunk
-        
-    redirect_to :action => 'notfound' unless @chunk_version
+    if @chunk_version then
+      if @chunk_version.chunk.mime_type then
+        mime_type = @chunk_version.chunk.mime_type.mime_type
+      else
+        mime_type = "text/html"
+      end
+      send_data @chunk_version.content, :filename => @params[:path].pop, :type => mime_type, :disposition => 'inline' if @chunk_version && @chunk_version.chunk
+    else 
+      render :partial => "notfound", :status => 404
+    end
   end
   
-  # 404 action
-  def notfound
-  end
+  
 end
