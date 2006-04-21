0dffc9bda6b1ae7432fce13220bc1e6a7abc32b2
Daniel Siemssen <daniel@nospam.thanks.dr-siemssen.de>
2006-04-21 17:36:13 +0000

    Hope this fixes the issues with Xinha.
    git-svn-id: https://svn.versiondude.net/railfrog/cms@120 02240113-d30a-0410-b920-55b7e5eb95ec

--- trunk/app/controllers/application.rb
+++ trunk/app/controllers/application.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base  
-  # Change this to your RailFrog installation directory with no trailing forward slash
-  # ToDo: Automatic Extraction from ENV
-  $INSTALLATION_BASE_URL = "http://localhost:3000"
-  
   helper_method :logged_in?
   
   # Check a User is Logged In
