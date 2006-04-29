b45f88dc6073498acd20114a6c6369efb9a6189e
Yury Kotlyarov <yura@nospam.thanks.brainhouse.ru>
2006-04-29 21:18:17 +0000

    (almost) Rails Engines ready. TODO solve problems with static content such as javascripts, stylesheets, images
    git-svn-id: https://svn.versiondude.net/railfrog/cms@130 02240113-d30a-0410-b920-55b7e5eb95ec

--- trunk/app/controllers/application.rb
+++ trunk/app/controllers/application.rb
@@ -1,21 +1,5 @@
 class ApplicationController < ActionController::Base  
-  helper_method :logged_in?
-  
-  # Check a User is Logged In
-  def logged_in?
-    session[:user_id]
-  end
+  include Railfrog
 
-  # Ensure a User is Logged In
-  def ensure_logged_in
-    return true if logged_in?
-    flash[:error] = 'Please Log In'
-    redirect_to_login
-  end
-  
-  # Re-Direct an Un-Authenticated User to Login
-  # ToDo: Return URI
-  def redirect_to_login
-    redirect_to :controller => 'users', :action => 'login'
-  end
+  model :user
 end
