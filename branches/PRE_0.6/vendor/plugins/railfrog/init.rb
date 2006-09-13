d8be3ea0b3a82061f2c12ac92decdc6c7e6d423b
Daniel Siemssen <daniel@nospam.thanks.dr-siemssen.de>
2006-09-13 10:30:12 +0000

    Plugins table will now be created automatically on startup if not present.
    git-svn-id: https://svn.versiondude.net/railfrog/cms@197 02240113-d30a-0410-b920-55b7e5eb95ec

--- branches/PRE_0.6/vendor/plugins/railfrog/init.rb
+++ branches/PRE_0.6/vendor/plugins/railfrog/init.rb
@@ -2,13 +2,9 @@ require 'hash_extension'
 require 'plugin_system'
 require File.join(RAILS_ROOT, 'vendor', 'plugins', 'railfrog', 'app', 'models', 'plugin.rb')
 
-# Start Plugin System
+unless Plugin.table_exists?
+  load File.dirname(__FILE__) + '/db/plugins_table.rb'
+end
+
 rf_plugins_root = File.join(RAILS_ROOT, 'vendor', 'railfrog_plugins')
-#PluginSystem::Base.startup(config, rf_plugins_root)
 PluginSystem::Base.startup(rf_plugins_root)
-
-# Add Railfrog Controllers to controller_path
-#config.controller_paths.concat [File.join(RAILS_ROOT, 'vendor', 'plugins', 'railfrog_core', 'app', 'controllers')]
-
-# Do stuff...
-#require 'rf_experiments'
