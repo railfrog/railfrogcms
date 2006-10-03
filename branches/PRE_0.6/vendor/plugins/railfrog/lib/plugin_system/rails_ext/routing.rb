0fab1086bae29335869c3dbc7d2c07ea634e0255
Daniel Siemssen <daniel@nospam.thanks.dr-siemssen.de>
2006-10-03 23:05:13 +0000

    Added the lost routing.rb that should have already been in changeset:213
    git-svn-id: https://svn.versiondude.net/railfrog/cms@220 02240113-d30a-0410-b920-55b7e5eb95ec

---
+++ branches/PRE_0.6/vendor/plugins/railfrog/lib/plugin_system/rails_ext/routing.rb
@@ -0,0 +1,21 @@
+module ActionController
+  module Routing
+    class RouteSet
+      def draw_more
+        yield Mapper.new(self)
+        named_routes.install
+      end
+      
+      alias :railfrog_original_load_routes! :load_routes!
+      def load_routes!
+        railfrog_original_load_routes!
+        default_route = routes.pop if routes.last.segments.map(&:to_s).join('') == '/:controller/:action/:id/'
+        ::PluginSystem::Instance.started_plugins.load_order.reverse.each do |plugin|
+          routes_file = File.join(plugin.path_to_gem, 'config', 'routes.rb')
+          load routes_file if File.file? routes_file
+        end
+        routes << default_route if default_route
+      end
+    end
+  end
+end
