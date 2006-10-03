d122233ec70ac1414dd85887646096907101b027
Daniel Siemssen <daniel@nospam.thanks.dr-siemssen.de>
2006-10-03 22:37:13 +0000

    Ups, I forgot to require the new railfrog_resources.rb
    git-svn-id: https://svn.versiondude.net/railfrog/cms@219 02240113-d30a-0410-b920-55b7e5eb95ec

--- branches/PRE_0.6/vendor/plugins/railfrog/init.rb
+++ branches/PRE_0.6/vendor/plugins/railfrog/init.rb
@@ -8,7 +8,16 @@ edge_rails = false
 
 if edge_rails
   require 'plugin_system'
-
-  # Start Plugin System 
+  require 'railfrog_resources'
+  
+  # FIXME: REMOVE THIS
+  if RAILS_ENV == 'test'
+    PluginSystem::Instance.installed_plugins.each do |plugin|
+      plugin.enable if plugin.disabled?
+    end
+  end
+  ####################
+  
+  # Start Plugin System
   PluginSystem::Instance.start(config)
 end
