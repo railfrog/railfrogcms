bb9edc484d35b4942e9d41293d9553fb1ff95633
Daniel Siemssen <daniel@nospam.thanks.dr-siemssen.de>
2006-10-11 21:29:01 +0000

    Make Routing extension work with an empty config/routes.rb
    git-svn-id: https://svn.versiondude.net/railfrog/cms@229 02240113-d30a-0410-b920-55b7e5eb95ec

--- branches/PRE_0.6/vendor/plugins/railfrog/lib/plugin_system/rails_ext/routing.rb
+++ branches/PRE_0.6/vendor/plugins/railfrog/lib/plugin_system/rails_ext/routing.rb
@@ -9,7 +9,7 @@ module ActionController
       alias :railfrog_original_load_routes! :load_routes!
       def load_routes!
         railfrog_original_load_routes!
-        default_route = routes.pop if routes.last.segments.map(&:to_s).join('') == '/:controller/:action/:id/' # TODO: remove this ...
+        default_route = routes.pop if !routes.empty? && routes.last.segments.map(&:to_s).join('') == '/:controller/:action/:id/' # TODO: remove this ...
         ::PluginSystem::Instance.started_plugins.load_order.reverse.each do |plugin|
           routes_file = File.join(plugin.path_to_gem, 'config', 'routes.rb')
           load routes_file if File.file? routes_file
