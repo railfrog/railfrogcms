75c9a9d5dcf8fb459465557d8398f72412cf66c1
Daniel Siemssen <daniel@nospam.thanks.dr-siemssen.de>
2007-01-03 00:30:55 +0000

    Only start PluginSystem if Rails 1.2 / Edge is installed
    git-svn-id: https://svn.versiondude.net/railfrog/cms@251 02240113-d30a-0410-b920-55b7e5eb95ec

--- branches/PRE_0.6/vendor/plugins/railfrog/init.rb
+++ branches/PRE_0.6/vendor/plugins/railfrog/init.rb
@@ -1,13 +1,15 @@
 require 'hash_extension'
 require 'plugin_system'
 
-# FIXME: REMOVE THIS
-if RAILS_ENV == 'test'
-  PluginSystem::Instance.installed_plugins.each do |plugin|
-    plugin.enable if plugin.disabled?
+if Dependencies.respond_to?(:autoloaded_constants) # I just try to figure out if Edge Rails is installed
+  # FIXME: REMOVE THIS
+  if RAILS_ENV == 'test'
+    PluginSystem::Instance.installed_plugins.each do |plugin|
+      plugin.enable if plugin.disabled?
+    end
   end
+  ####################
+  
+  # Start Plugin System
+  PluginSystem::Instance.start(config)
 end
-####################
-
-# Start Plugin System
-PluginSystem::Instance.start(config)
