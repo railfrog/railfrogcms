5a298f4769d894314efd75fa9592c05b2f6bf438
Thomas Nichols <nichols7@nospam.thanks.gmail.com>
2007-06-16 11:41:37 +0000

    Add new mime_type_str column and set values for all existing chunks
    git-svn-id: https://svn.versiondude.net/railfrog/cms@324 02240113-d30a-0410-b920-55b7e5eb95ec

---
+++ trunk/db/migrate/005_store_mime_type_string_on_chunk.rb
@@ -0,0 +1,17 @@
+class StoreMimeTypeStringOnChunk < ActiveRecord::Migration
+  def self.up
+    add_column :chunks, :mime_type_str, :string, :default => "", :limit => 100
+    # Set string mime type for each chunk
+    all_chunks = Chunk.find(:all)
+    all_chunks.each do |chunk|
+      if chunk.mime_type_str.nil? || chunk.mime_type_str.empty?
+        chunk.mime_type_str = chunk.mime_type.mime_type
+        chunk.save!
+      end
+    end
+  end
+
+  def self.down
+    drop_column :chunks, :mime_type_str
+  end
+end
