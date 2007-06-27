baeb27efc35e38bf8c92e2b092b178c8728ae715
Yury Kotlyarov <yura@nospam.thanks.brainhouse.ru>
2007-06-27 15:49:42 +0000

    adding helper test plugin
    git-svn-id: https://svn.versiondude.net/railfrog/cms@336 02240113-d30a-0410-b920-55b7e5eb95ec

---
+++ trunk/test/unit/helpers/application_helper_test.rb
@@ -0,0 +1,13 @@
+require File.dirname(__FILE__) + '/../../test_helper'
+
+class ApplicationHelperTest < HelperTestCase
+
+  include ApplicationHelper
+
+  #fixtures :users, :articles
+
+  def setup
+    super
+  end
+  
+end
