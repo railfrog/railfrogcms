384af44b48f25b7bb34f20d98ecd2e02153f16e1
Thomas Nichols <nichols7@nospam.thanks.gmail.com>
2007-09-04 10:57:28 +0000

    svn reorg
    git-svn-id: https://svn.versiondude.net/railfrog/cms@406 02240113-d30a-0410-b920-55b7e5eb95ec

--- tags/rel_0-5-3/app/controllers/application.rb
+++
@@ -1,5 +0,0 @@
-class ApplicationController < ActionController::Base
-  include Railfrog
-  helper :prototype_window_class
-  model :user
-end
--- tags/rel_0-5-3/test/unit/mime_type_test.rb
+++
@@ -1,9 +0,0 @@
-require File.dirname(__FILE__) + '/../test_helper'
-
-class MimeTypeTest < Test::Unit::TestCase
-  fixtures :mime_types
-
-  # Replace this with your real tests.
-  def test_truth
-  end
-end
