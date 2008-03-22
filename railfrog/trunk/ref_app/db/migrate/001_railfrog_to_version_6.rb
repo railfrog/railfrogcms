bccfdd6af8a09504a9e91392a0da11a6c1b69294
Yury Kotlyarov <yura@nospam.thanks.brainhouse.ru>
2008-03-22 13:58:55 +0000

    dropping old ref app
    git-svn-id: https://svn.versiondude.net/railfrog/cms@438 02240113-d30a-0410-b920-55b7e5eb95ec

--- railfrog/trunk/ref_app/db/migrate/001_railfrog_to_version_6.rb
+++
@@ -1,9 +0,0 @@
-class RailfrogToVersion6 < ActiveRecord::Migration
-  def self.up
-    Rails.plugins["railfrog"].migrate(6)
-  end
-
-  def self.down
-    Rails.plugins["railfrog"].migrate(0)
-  end
-end
--- railfrog/trunk/ref_app/vendor/plugins/railfrog/test/railfrog_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test/unit'
-
-class RailfrogTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  def test_this_plugin
-    flunk
-  end
-end
