0a295be1d320cc52714fcd71ddb3298722ad4eca
Yury Kotlyarov <yura@nospam.thanks.brainhouse.ru>
2008-04-13 18:53:40 +0000

    polising folders code
    git-svn-id: https://svn.versiondude.net/railfrog/cms@476 02240113-d30a-0410-b920-55b7e5eb95ec

--- railfrog/trunk/app/helpers/railfrog_admin_helper.rb
+++ railfrog/trunk/app/helpers/railfrog_admin_helper.rb
@@ -12,7 +12,7 @@ module RailfrogAdminHelper
     tree = "<div id=\"tree_#{parent_id}\"><ul>"
     for map in mappings[parent_id] do
       tree += '<li>'
-      tree += link_to_function('', visual_effect(:toggle_slide, "tree_#{map.id}", :duration => 0.5)+" if (Element.hasClassName('link_tree_#{map.id}', 'expand')) { Element.addClassName('link_tree_#{map.id}', 'collapse'); Element.removeClassName('link_tree_#{map.id}', 'expand'); } else { Element.addClassName('link_tree_#{map.id}', 'expand'); Element.removeClassName('link_tree_#{map.id}', 'collapse'); }", :class => (parent_id == 0 ? 'collapse':'expand'), :id => "link_tree_#{map.id}") if !map.rgt.nil? && !map.lft.nil? && ((map.all_children.size > 0) || map.chunk_id.nil?)
+      tree += link_to_function('', visual_effect(:toggle_slide, "tree_#{map.id}", :duration => 0.5)+" if ($('link_tree_#{map.id}').hasClassName('expand')) { $('link_tree_#{map.id}').addClassName('collapse').removeClassName('expand'); } else { $('link_tree_#{map.id}').addClassName('expand').removeClassName('collapse'); }", :class => ( parent_id == 0 ? 'collapse' : 'expand' ), :id => "link_tree_#{map.id}") if !map.rgt.nil? && !map.lft.nil? && ((map.all_children.size > 0) || map.chunk_id.nil?)
       tree += link_to_remote("#{h((map.path_segment.length > 0 ? map.path_segment : '/'))}", 
         { :url => { :action => 'show', :mapping_id => map.id }}, 
 	((map.is_internal) ? {:class => 'internal'} : {}) )
@@ -20,8 +20,11 @@ module RailfrogAdminHelper
       tree += '</li>'
     end
     tree += '</ul></div>'
-    if parent_id > 1
-      tree += javascript_tag("Element.hide('tree_#{parent_id}')") 
+
+    if parent_id == 0
+      tree += javascript_tag("$('tree_#{SiteMapping.find_root.id}').show()") 
+    else
+      tree += javascript_tag("$('tree_#{parent_id}').hide()") 
     end
 
     tree
